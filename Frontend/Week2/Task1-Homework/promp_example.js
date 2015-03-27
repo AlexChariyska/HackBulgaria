66ac6e5444074fdd5c6e6dcbfca2502a1306a98e
Alexandra <alex.chariyska@gmail.com>
2015-03-27 11:47:39 +0200

    Hangman bug fixed

--- Frontend/Week2/Task1-Homework/promp_example.js
+++ Frontend/Week2/Task1-Homework/promp_example.js
@@ -4,12 +4,12 @@ var word = "";
 //Starting
 prompt.start();
 getWord();
-var lifes= 10;
+var lifes = 10;
 
 function getWord() {
     prompt.get(['word'], function (err, result) {
         word = result.word;
-        var hidden =hideWord(word);
+        var hidden = hideWord(word);
         console.log(hidden);
         guessLetter(hidden);
         if (err) {
@@ -22,24 +22,24 @@ function guessLetter(hidden) {
     prompt.get(['letter'], function (err, result) {
         console.log(word);
         var letter = result.letter;
-        if (word.indexOf(letter) == -1){
+        if (word.indexOf(letter) == -1) {
             lifes--;
             console.log("The word doesn't contain the letter. Try again");
             console.log("Lifes: " + lifes);
             console.log(hidden);
             guessLetter(hidden);
-        }else{
+        } else {
             var index = word.indexOf(letter);
             hidden = hidden.replaceAt(index, letter);
 
-            if(word === hidden){
+            if (word === hidden) {
                 console.log("You win!");
-            }else{
+            } else {
                 console.log(hidden);
                 guessLetter(hidden);
             }
         }
-        if(lifes == 0 ){
+        if (lifes == 0) {
             console.log("You lose!");
         }
 
@@ -55,15 +55,36 @@ function onErr(err) {
     return 1;
 }
 
-function hideWord(word){
-    var firstLetter = word.slice(0,1);
+function hideWord(word) {
+    var firstLetter = word.slice(0, 1);
+    var firstLettersArr = charPos(word, firstLetter);
     var lastLetter = word.slice(-1);
-    var hidden = Array(word.length-1).join(" _ ");
-    return firstLetter + hidden + lastLetter;
+    var lastLettersArr = charPos(word, lastLetter);
+
+    var hidden = Array(word.length).join("_").split("");
+    for (var i = 0; i < firstLettersArr.length; i++) {
+        hidden[firstLettersArr[i]] = firstLetter;
+    }
+    for (var i = 0; i < lastLettersArr.length; i++) {
+        hidden[lastLettersArr[i]] = lastLetter;
+    }
+    return hidden.join("");
 }
 
-String.prototype.replaceAt=function(index, char) {
+function charPos(str, char) {
+    return str
+        .split("")
+        .map(function (c, i) {
+            if (c == char) return i;
+        })
+        .filter(function (v) {
+            return v >= 0;
+        });
+}
+
+String.prototype.replaceAt = function (index, char) {
     var a = this.split("");
     a[index] = char;
     return a.join("");
-}
+};
+
